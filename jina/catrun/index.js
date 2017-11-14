72f96fd277df34f05526be75a0556ef3e265b318
JINAA <10rlawlsk@gmail.com>
2017-11-14 12:30:43 +0900

    Jina proejct Catrun update 1. Bring the cat on the background 2. Now the cat can move to the upside

--- jina/catrun/index.js
+++ jina/catrun/index.js
@@ -1,23 +1,64 @@
-var cat = document.getElementById('cat1');
 var canvas = document.getElementById('myCanvas');
 var ctx = canvas.getContext("2d");
 
+//고양이 그림 부르기
+var catImage = [];
+catImage[0] = new Image();
+catImage[1] = new Image();
+catImage[0].src = "cat1.png";
+catImage[1].src = "cat2.png";
 
-var catImage = ["cat1.png","cat2.png"];
-var imgidx = 0;
-var catX = canvas.width / 2
+//고양이 사이즈와 초기좌표
+var catwidth = 90;
+var catheight = 90;
+var catX = 100;
+var catY = 220;
 
+//고양이 움직임
+var dx = 2;
+var dy = -130;
+
+//고양이 로드시키기
+var catImageLoad = false;
+catImage.onload = function () {
+	catImage = true;
+};
+
+
+//배경그리기	
 function drawbg() {
 	ctx.fillStyle = "lavender";
 	ctx.fillRect(10,10,500,500);
 }
-drawbg();
 
-function runcat() {
-	cat.setAttribute("src",catImage[imgidx]);
-	imgidx++;
-	if (imgidx >= catImage.length) {
-		imgidx = 0;
+//고양이 점프
+function jump() {
+	if (catY > canvas.height / 2) {
+		catY += dy;
+		return catY;
 	}
 }
-setInterval(runcat,700);
+
+//고양이 그리기
+var count = 0;
+function drawcat() {
+	count++;
+	if (count >= 3){
+		ctx.drawImage(catImage[0],catX,catY,catwidth,catheight);
+	} else {
+		ctx.drawImage(catImage[1],catX,catY,catwidth,catheight);
+	}
+
+	if (count == 6)
+		count = 0;
+}
+
+//그린이미지실행
+function drawAll() {
+	drawbg();
+	drawcat();
+}
+
+
+
+setInterval(drawAll, 60);
