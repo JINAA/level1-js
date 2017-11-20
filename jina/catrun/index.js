3e156f95efd3d76bccf87b491dc34209855b3b0d
JINAA <10rlawlsk@gmail.com>
2017-11-20 10:20:36 +0900

    Update project Catrun Implement more features Now the background is moving.(but moving to the other side)

--- jina/catrun/index.js
+++ jina/catrun/index.js
@@ -7,33 +7,46 @@ catImage[0] = new Image();
 catImage[1] = new Image();
 catImage[0].src = "cat1.png";
 catImage[1].src = "cat2.png";
-
 //배경 부르기
 var bg = new Image();
 bg.src = 'bg.png';
 
+//장애물 변수
+var objW = 50;
+var objH = 50;
+
 //배경 변수
 var sizeX = 550;
 var sizeY = 350;
-var speed = 40;
-var yy = - 4.5;
-var ddx = 0.75;
+var speed = 10;
+var yy = 0;
+var ddx = - 0.75;
 var xx = 0;
-
 //배경그리기
 function drawbg() {
-	ctx.clearRect(0,0,canvas.width,canvas.height);
+ 	ctx.clearRect(0,0,canvas.width,canvas.height);
+	
+	ctx.drawImage(bg,xx - canvas.width,yy,canvas.width,canvas.height);
 
-		if (xx > sizeX) { xx = sizeX - canvas.width}
-		if (xx > sizeX - canvas.width) {
-			ctx.drawImage(bg,xx - canvas.width,yy,canvas.width,canvas.height);
-		}
+ 		if (xx > sizeX) { xx = sizeX - canvas.width}
+ 		if (xx < sizeX + canvas.width) {
+ 			ctx.drawImage(bg,xx + canvas.width,yy,canvas.width,canvas.height);
+ 		}
 
-	ctx.drawImage(bg,xx,yy,canvas.width,canvas.height);
+ 	ctx.drawImage(bg,xx,yy,canvas.width,canvas.height);
 
-	xx += ddx;
+ 	xx += ddx;
 }
 setInterval(drawbg, speed);
+
+//장애물 그리기
+function drawobj() {
+	ctx.beginPath();
+	ctx.rect(400,265,objW,objH);
+	ctx.fillStyle = "purple";
+	ctx.fill();
+	ctx.closePath();
+}
 
 //고양이 사이즈와 초기좌표
 var catwidth = 90;
@@ -53,29 +66,6 @@ function jump() {
 	dy = -7;
 }
 
-=======
-	
-//고양이 사이즈와 초기좌표
-var catwidth = 90;
-var catheight = 90;
-var catX = 100;
-var catY = 220;
-
-//고양이 움직임
-var dx = 0;
-var dy = -7;
-
-//고양이 중력
-var g = 0.2;
-
-//배경그리기	
-function drawbg() {
-	ctx.fillStyle = "lavender";
-	ctx.fillRect(10,10,canvas.width,canvas.height);
-}
-
-
->>>>>>> honux/jina
 //뛰는 고양이 그리기
 var count = 0;
 var idx = 0;
@@ -84,28 +74,18 @@ var delay = 10;
 function drawcat() {
 	ctx.clearRect(0,0,canvas.width,canvas.height);
 	drawbg();
-<<<<<<< HEAD
+	drawobj();
 
-=======
-	
->>>>>>> honux/jina
 	catX += dx;
 	dy = dy + g;
 	catY += + dy;
 
 	if (catX >= 500 || catX <= 0) {
 		dx = -dx;
-<<<<<<< HEAD
 	}
 	if(catY >= 230) {
 		catY = 230;
 	}
-=======
-	}
-	if(catY >= 200) {
-		catY = 200;
-	}
->>>>>>> honux/jina
 
 	count++;
 	if (count >= delay) {
@@ -116,23 +96,12 @@ function drawcat() {
 	count = 0;
 	}
 
-<<<<<<< HEAD
 	if (catY != 230) {
 		ctx.drawImage(catImage[0],catX,catY,catwidth,catheight);
 	} else {
 		ctx.drawImage(catImage[idx],catX,catY,catwidth,catheight);
 	}
 	requestAnimationFrame(drawcat);
-=======
-	if (catY != 200) {
-		ctx.drawImage(catImage[0],catX,catY,catwidth,catheight);	
-	} else {
-		ctx.drawImage(catImage[idx],catX,catY,catwidth,catheight);	
-
-	}
-	requestAnimationFrame(drawcat);
-	
->>>>>>> honux/jina
 }
 
 requestAnimationFrame(drawcat);
