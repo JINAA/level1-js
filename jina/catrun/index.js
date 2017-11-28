9a7c2ecbb29355bfc650acb46d7555cc4e3e338a
JINAA <10rlawlsk@gmail.com>
2017-11-28 13:48:42 +0900

    Complete project Catrun js source

--- jina/catrun/index.js
+++ jina/catrun/index.js
@@ -19,11 +19,7 @@ bg.src = 'bg.png';
 var objImage = new Image();
 objImage.src = "obj.png";
 //장애물 변수
-
 var Crab = {
-
-var crab = {
-
 	"sx":35,
 	"sy":460,
 	"w":225,
@@ -33,7 +29,6 @@ var crab = {
 	"width":70,
 	"height":70
 };
-
 
 //장애물 그리기
 var obji = 0;
@@ -81,69 +76,6 @@ function drawbg() {
 	ctx.drawImage(bg,0 - i + canvas.width,0,canvas.width, canvas.height);
 
 	if (i == canvas.width ) {
-
-// function crab(x,width,height) {
-// 	this.sx = 35;
-// 	this.sy = 460;
-// 	this.w = 225;
-// 	this.h = 300;
-// 	this.x = 600;
-// 	this.y = 250;
-// 	this.width = 70;
-// 	this.height = 70;
-// }
-// crab.prototype.drawobj = function() {
-// 	ctx.drawImage(objImage,this.sx,this.sy,this.w,this.h
-// 		,this.x,this.y,this.width,this.height);
-// 		this.x -= 3;
-// }
-// function newobj(n) {
-// 	var width = random(30) + 20;
-// 	var x = new crab(650,60,60)
-// }
-//장애물 그리기
-function drawobj() {
-	ctx.drawImage(objImage,crab.sx,crab.sy,crab.w,crab.h
-		,crab.x,crab.y,crab.width,crab.height);
-		crab.x -= 3;
-}
-
-//장애물 충돌감지
-function collision() {
-
-	if (crab.x > catX-15 && crab.x < catX+catwidth-15
-	&& crab.y > catY-15 && crab.y < catY+catheight-15) {
-				debugger;
-				// alert("GAME OVER");
-				document.location.reload();
-	}
-}
-//점수 계산
-var score = 0;
-function checkScore() {
-	score++;
-	ctx.font = "16px Arial";
-	ctx.fillStyle = "#0095DD";
-	ctx.font = "20px Verdana";
-	ctx.fillText("Score: " + score, 15, 30);
-
-}
-
-//배경 변수
-var sizeX = 550;
-var sizeY = 350;
-var ddx = - 0.75;
-var bgX = 0;
-var i = 0;
-//배경그리기
-function drawbg() {
-	i=i+2;
-	ctx.clearRect(0,0,canvas.width,canvas.height);
-	ctx.drawImage(bg,0 - i,0,sizeX, sizeY);
-	ctx.drawImage(bg,0 - i + sizeX,0,sizeX, sizeY);
-
-	if (i == sizeX ) {
-
 		i = 0;
 	}
 }
@@ -201,7 +133,6 @@ function jump() {
 	}
 }
 
-
 //일시정지 함수
 document.addEventListener("click",pause);
 var isRunning = true;
@@ -216,28 +147,14 @@ function pause() {
 //그리기
 function drawAll() {
 	ctx.clearRect(0,0,canvas.width,canvas.height);
-
-//그리기
-
-function drawAll() {
-
-	ctx.clearRect(0,0,canvas.width,canvas.height);
-
-
 	drawbg();
 	drawobj();
 	collision();
 	drawcat();
 	checkScore();
 
-
 	if(isRunning) {
 	requestAnimationFrame(drawAll);
 	}
 }
 drawAll();
-
-	requestAnimationFrame(drawAll);
-}
-requestAnimationFrame(drawAll);
-
